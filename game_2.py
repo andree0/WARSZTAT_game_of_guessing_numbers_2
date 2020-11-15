954e6e9323c90a08c4b2cca08fd39fe95b4f8955
andrzej <jonek.andree@gmail.com>
2020-11-15 14:50:31 +0100

    one_mistake

--- game_2.py
+++ game_2.py
@@ -1,26 +1,26 @@
-print("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbkach")
+print("Imagine number between 0 and 1000, I will guess the number in 10 tries")
 go = input("and press ENTER")
-min = 0
-max = 1000
+user_min = 0
+user_max = 1000
 answer_options = ['too big', 'too small', 'you win']
 
 while True:
-    guess = int((max - min) / 2) + min
+    guess = int((user_max - user_min) / 2) + user_min
 
-    print("Zgaduję: ", guess)
+    print(f"It is number {guess}")
 
-    answer = input("Jaka jest twoja odpowiedź (Too small/Too big/You win)")
+    answer = input()
     answer = answer.lower()
+    if user_min == user_max or user_min == guess - 1:
+        print('not cheat!')
+    else:
 
-    if answer not in answer_options:
-        print('zła odpowiedź')
-    elif answer == 'you win':
-        print("Wygrałeś!")
-        break
-    elif answer == 'too big':
-        max = guess
-    elif answer == 'too small':
-        min = guess
-
-    if min == max:
-        print('nie oszukuj!')
+        if answer not in answer_options:
+            print('wrong answer')
+        elif answer == 'you win':
+            print("You win!")
+            break
+        elif answer == 'too big':
+            user_max = guess
+        elif answer == 'too small':
+            user_min = guess
